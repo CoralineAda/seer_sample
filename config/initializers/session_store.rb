d824c6b0cdbf38ba5426eaf75f4b9b46171508ca
Andy Reilly <areilly@kentfirerfa.org>
2013-12-26 23:19:39 -0800

    updated for Rails 3/4

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,15 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying cookie session data integrity.
-# If you change this key, all old sessions will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.session = {
-  :key         => '_sample_session',
-  :secret      => 'e56cda156307f98e3f1e3903f92158510cbc2b55956026f2aaff5cba3604db72bad97d4002e9198f8015ed1e0ced4356857a40ef04426ba01865b424beddb921'
-}
-
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rake db:sessions:create")
-# ActionController::Base.session_store = :active_record_store
+SeerUpdate::Application.config.session_store :cookie_store, key: '_seer_update_session'
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,40 @@
-ActionController::Routing::Routes.draw do |map|
-  map.area_chart '/area_chart', :controller => 'home', :action => 'area_chart'
-  map.bar_chart '/bar_chart', :controller => 'home', :action => 'bar_chart'
-  map.column_chart '/column_chart', :controller => 'home', :action => 'column_chart'
-  map.gauge '/gauge', :controller => 'home', :action => 'gauge'
-  map.line_chart '/line_chart', :controller => 'home', :action => 'line_chart'
-  map.pie_chart '/pie_chart', :controller => 'home', :action => 'pie_chart'
-  map.geo_chart '/geo_chart', :controller => 'home', :action => 'geo_chart'
-  map.root :controller => "home"
-  map.connect ':controller/:action/:id'
-  map.connect ':controller/:action/:id.:format'
+
+SeerUpdate::Application.routes.draw do
+
+#  map.area_chart '/area_chart', :controller => 'home', :action => 'area_chart'
+
+get 'area_chart' => 'home#area_chart'
+
+#  map.bar_chart '/bar_chart', :controller => 'home', :action => 'bar_chart'
+
+get 'bar_chart' => 'home#bar_chart'
+
+#  map.column_chart '/column_chart', :controller => 'home', :action => 'column_chart'
+
+get 'column_chart' => 'home#column_chart'
+
+#  map.gauge '/gauge', :controller => 'home', :action => 'gauge'
+
+get 'gauge' => 'home#gauge'
+
+#  map.line_chart '/line_chart', :controller => 'home', :action => 'line_chart'
+
+get 'line_chart' => 'home#line_chart'
+
+#  map.pie_chart '/pie_chart', :controller => 'home', :action => 'pie_chart'
+
+get 'pie_chart' => 'home#pie_chart'
+
+#  map.geo_chart '/geo_chart', :controller => 'home', :action => 'geo_chart'
+
+get 'geo_chart' => 'home#geo_chart'
+
+#  map.root :controller => "home"
+
+root :to => 'home#index'
+
+#  map.connect ':controller/:action/:id'
+
+#  map.connect ':controller/:action/:id.:format'
+
 end
